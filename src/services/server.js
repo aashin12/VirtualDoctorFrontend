0968987af56f382467480d1c9f63ee20edffc4f8
aashin12 <aashinkb369@gmail.com>
2025-07-18 06:55:49 +0530

    frontend test2

--- src/services/server.js
+++ src/services/server.js
@@ -1,1 +1,3 @@
-export const serverUrl = 'https://virtualdoctorbackend.onrender.com'
+ export const serverUrl = 'https://virtualdoctorbackend.onrender.com'
+
+// export const serverUrl = 'http://localhost:4000'
