4c098421ac3414f635e4fe0185b659f788f5ae05
aashin12 <aashinkb369@gmail.com>
2025-07-18 06:17:48 +0530

    frontend test

--- src/services/allApi.js
+++ src/services/allApi.js
@@ -1,6 +1,7 @@
 import { commonApi } from "./commonApi"
 import { serverUrl } from "./serverUrl"
 
+
 //register
 export const registerApi = async(reqBody)=>{
     return await commonApi('POST',`${serverUrl}/users/register`,reqBody)
